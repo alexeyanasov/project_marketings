fe9db0feb4496e7e93af2ae41f3a0bcae0525c27
Alexey <alexey.anasov@yandex.by>
2021-07-15 20:08:01 +0300

    +slider+adaptiv

--- js/script.js
+++ js/script.js
@@ -6,6 +6,14 @@ $(function () {
       '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
     nextArrow:
       '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
 
   // burger-menu
